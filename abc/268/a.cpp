26dd0f10004043356c5c760a01d24c45db318a07
Shion <shion24stat@gmail.com>
2022-10-19 00:23:21 +0900

    abc268

---
+++ abc/268/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+
+    set<int> s = {a, b, c, d, e};
+    
+    cout << s.size() << endl;
+
+    return 0;
+}
---
+++ abc/268/b.py
@@ -0,0 +1,6 @@
+s = input()
+t = input()
+
+pos = t.find(s)
+
+print("Yes") if pos == 0 else print("No")
