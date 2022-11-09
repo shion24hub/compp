f7ea34ca89f063959b84ca4b73e4c619824e9ba2
Shion <shion24stat@gmail.com>
2022-11-09 18:26:51 +0900

    test

---
+++ tessoku/f/a/1.py
@@ -0,0 +1,14 @@
+import math
+import itertools
+import sys
+from copy import deepcopy
+
+sys.setrecursionlimit(10**6)
+
+
+def solve() : 
+    n = int(input())
+    print(n * n)
+
+if __name__ == '__main__' :
+    solve()
---
+++ tessoku/f/b/1.py
@@ -0,0 +1,14 @@
+import math
+import itertools
+import sys
+from copy import deepcopy
+
+sys.setrecursionlimit(10**6)
+
+
+def solve() : 
+    a, b = map(int, input().split())
+    print(a + b)
+
+if __name__ == '__main__' :
+    solve()
