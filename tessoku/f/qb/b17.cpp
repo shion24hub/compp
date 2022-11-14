30ccea901e5aa5ba99cafcd0f0f623a5568f2c47
Shion <shion24stat@gmail.com>
2022-11-15 06:46:30 +0900

    to b17

--- tessoku/f/qb/b17.cpp
+++ tessoku/f/qb/b17.cpp
@@ -68,32 +68,9 @@ int binsll(int left, int right, long long key, vector<long long> vec) {
 int main() {
 
     int n; cin >> n;
-    vector<int> a(n); for (int i = 1; i < n; i++) cin >> a[i];
-    vector<int> b(n); for (int i = 2; i < n; i++) cin >> b[i];
+    vector<int> h(n); rep(i, n) cin >> h[i];
 
-    vector<int> dp(n); dp[0] = 0; dp[1] = a[1];
-    for (int i = 2; i < n; i++) {
-        dp[i] = min(dp[i-1] + a[i], dp[i-2] + b[i]);
-    }
-
-    vector<int> ans; ans.push_back(n);
-    int place = n-1;
-    while(true) {
-        if (place == 0) break;
-        if (dp[place-1] + a[place] == dp[place]) {
-            ans.push_back(place);
-            place = place-1;
-        }
-        else if (dp[place-2] + b[place] == dp[place]) {
-            ans.push_back(place-1);
-            place = place-2;
-        }
-    }
-    reverse(ans.begin(), ans.end());
-
-    cout << ans.size() << endl;
-    rep(i, ans.size()) cout << ans[i] << " ";
-    cout << endl;
+    
 
     return 0;
 }
