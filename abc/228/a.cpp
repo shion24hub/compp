510f559f0764f2fac976324c8038c9d6144a98e4
Shion <shion24stat@gmail.com>
2022-11-25 04:20:49 +0900

    abc 215, 216, 228 abc

---
+++ abc/228/a.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = n-1; i >= 0; i--)
+
+vector<char> UPPERCASE = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+vector<char> LOWERCASE = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+void print() {
+  cout << endl;
+}
+
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(tail) != 0) cout << ' ';
+  print(forward<Tail>(tail)...);
+}
+
+template <class T>
+void print(vector<T> &vec) {
+  for (auto& a : vec) {
+    cout << a;
+    if (&a != &vec.back()) cout << ' ';
+  }
+  cout << endl;
+}
+
+template <class T>
+void print(vector<vector<T>> &df) {
+  for (auto& vec : df) {
+    print(vec);
+  }
+}
+
+//binary search (int)
+int binsi(int left, int right, int key, vector<int> vec) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (key == vec[mid]) return mid;
+        else if (key < vec[mid]) right = mid - 1;
+        else if (vec[mid] < key) left = mid + 1; 
+    }
+    return -1;
+}
+
+//binary search (long long)
+int binsll(int left, int right, long long key, vector<long long> vec) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (key == vec[mid]) return mid;
+        else if (key < vec[mid]) right = mid - 1;
+        else if (vec[mid] < key) left = mid + 1; 
+    }
+    return -1;
+}
+
+int main() {
+
+    int s, t, x; cin >> s >> t >> x;
+
+    vector<bool> day(24, false);
+    int len;
+    if (t < s) {
+        t += 24;
+        len = t - s + 1;
+    }
+    else {
+        len = t - s - 1;
+    }
+    // cout << "len " << len << endl;
+
+    for (int i = 0; i < len; i++) {
+        day[(s+i)%24] = true;
+    }
+
+    // rep(i, 24) cout << day[i] << " ";
+    // cout << endl; 
+
+    if (day[(x+1)%24]) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}
---
+++ abc/228/b.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = n-1; i >= 0; i--)
+
+vector<char> UPPERCASE = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+vector<char> LOWERCASE = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+void print() {
+  cout << endl;
+}
+
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(tail) != 0) cout << ' ';
+  print(forward<Tail>(tail)...);
+}
+
+template <class T>
+void print(vector<T> &vec) {
+  for (auto& a : vec) {
+    cout << a;
+    if (&a != &vec.back()) cout << ' ';
+  }
+  cout << endl;
+}
+
+template <class T>
+void print(vector<vector<T>> &df) {
+  for (auto& vec : df) {
+    print(vec);
+  }
+}
+
+//binary search (int)
+int binsi(int left, int right, int key, vector<int> vec) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (key == vec[mid]) return mid;
+        else if (key < vec[mid]) right = mid - 1;
+        else if (vec[mid] < key) left = mid + 1; 
+    }
+    return -1;
+}
+
+//binary search (long long)
+int binsll(int left, int right, long long key, vector<long long> vec) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (key == vec[mid]) return mid;
+        else if (key < vec[mid]) right = mid - 1;
+        else if (vec[mid] < key) left = mid + 1; 
+    }
+    return -1;
+}
+
+int main() {
+
+    int n, x; cin >> n >> x;
+    vector<int> a(n+1); for (int i = 1; i <= n; i++) cin >> a[i];
+
+    vector<bool> f(n+1, false);
+    int next = x;
+    while (true) {
+        if (!f[next]) {
+            f[next] = true;
+            next = a[next];
+        }
+        else break;
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
+        if (f[i]) ans ++;
+    }
+    cout << ans << endl;
+
+    return 0;
+}
