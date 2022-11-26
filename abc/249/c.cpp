5adbeaf9e37d379b34b944c9580ecccd80c59175
Shion <shion24stat@gmail.com>
2022-11-26 11:07:07 +0900

    abc 249 bc

---
+++ abc/249/c.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = n-1; i >= 0; i--)
+
+vector<char> UPPERCASE = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+vector<char> LOWERCASE = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+void print() {
+  cout << endl;
+}
+
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(tail) != 0) cout << ' ';
+  print(forward<Tail>(tail)...);
+}
+
+template <class T>
+void print(vector<T> &vec) {
+  for (auto& a : vec) {
+    cout << a;
+    if (&a != &vec.back()) cout << ' ';
+  }
+  cout << endl;
+}
+
+template <class T>
+void print(vector<vector<T>> &df) {
+  for (auto& vec : df) {
+    print(vec);
+  }
+}
+
+//binary search (int)
+int binsi(int left, int right, int key, vector<int> vec) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (key == vec[mid]) return mid;
+        else if (key < vec[mid]) right = mid - 1;
+        else if (vec[mid] < key) left = mid + 1; 
+    }
+    return -1;
+}
+
+//binary search (long long)
+int binsll(int left, int right, long long key, vector<long long> vec) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (key == vec[mid]) return mid;
+        else if (key < vec[mid]) right = mid - 1;
+        else if (vec[mid] < key) left = mid + 1; 
+    }
+    return -1;
+}
+
+int main() {
+
+    int n, k; cin >> n >> k;
+    vector<string> s(n); rep(i, n) cin >> s[i];
+
+    int max = 0;
+    for (int i = 0; i < (1 << n); i++) {
+        map<char, int> counter;
+        for (int j = 0; j < n; j++) {
+            int div = (1 << j);
+            if ((i / div) % 2 == 1)  {
+                for (int k = 0; k < s[j].size(); k++) counter[s[j][k]] += 1;
+            }
+        }
+        int temp = 0;
+        for (auto c : counter) {
+            if (c.second == k) temp++; 
+        }
+        if (max < temp) max = temp;
+    }
+
+    cout << max << endl;
+
+    return 0;
+}
