30fd14501872dd16761609340a1ed4427c09adff
Shion <shion24stat@gmail.com>
2022-11-29 09:58:29 +0900

    add many

---
+++ codeforces/d4/835/a.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = n-1; i >= 0; i--)
+
+vector<char> UPPERCASE = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+vector<char> LOWERCASE = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+void print() {
+  cout << endl;
+}
+
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(tail) != 0) cout << ' ';
+  print(forward<Tail>(tail)...);
+}
+
+template <class T>
+void print(vector<T> &vec) {
+  for (auto& a : vec) {
+    cout << a;
+    if (&a != &vec.back()) cout << ' ';
+  }
+  cout << endl;
+}
+
+template <class T>
+void print(vector<vector<T>> &df) {
+  for (auto& vec : df) {
+    print(vec);
+  }
+}
+
+class UnionFind {
+   public:
+   int par[100009];
+   int siz[100009];
+
+   void init(int N) {
+       for (int i = 1; i <= N; i++) par[i] = -1;
+       for (int i = 1; i <= N; i++) siz[i] = 1;
+   }
+
+   int root(int x) {
+       while (true) {
+           if (par[x] == -1) break;
+           x = par[x];
+       }
+       return x;
+   }
+
+   void unite(int u, int v) {
+       int rootU = root(u);
+       int rootV = root(v);
+       if (rootU == rootV) return;
+       if (siz[rootU] < siz[rootV]) {
+           par[rootU] = rootV;
+           siz[rootV] = siz[rootU] + siz[rootV];
+       }
+       else {
+           par[rootV] = rootU;
+           siz[rootU] = siz[rootU] + siz[rootV];
+       }
+   }
+
+   bool same(int u, int v) {
+       if (root(u) == root(v)) return true;
+       return false;
+   }
+};
+
+int main() {
+
+    int t; cin >> t;
+    int a, b, c;
+    int maxn, minn;
+    for (int i = 0; i < t; i ++) {
+        cin >> a >> b >> c;
+        maxn = max(max(a, b), c);
+        minn = min(min(a, b), c);
+        if (a != maxn && a != minn) {cout << a << endl; continue;}
+        if (b != maxn && b != minn) {cout << b << endl; continue;}
+        cout << c << endl;
+    }
+
+    return 0;
+}
