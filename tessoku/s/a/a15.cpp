b05ea1084ea3f382be271420ac4750695439eaa9
Shion <shion24stat@gmail.com>
2022-12-02 10:37:45 +0900

    add many

---
+++ tessoku/s/a/a15.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = n-1; i >= 0; i--)
+
+vector<char> UPPERCASE = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+vector<char> LOWERCASE = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+void print() {
+  cout << endl;
+}
+
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(tail) != 0) cout << ' ';
+  print(forward<Tail>(tail)...);
+}
+
+template <class T>
+void print(vector<T> &vec) {
+  for (auto& a : vec) {
+    cout << a;
+    if (&a != &vec.back()) cout << ' ';
+  }
+  cout << endl;
+}
+
+template <class T>
+void print(vector<vector<T>> &df) {
+  for (auto& vec : df) {
+    print(vec);
+  }
+}
+
+class UnionFind {
+   public:
+   int par[100009];
+   int siz[100009];
+
+   void init(int N) {
+       for (int i = 1; i <= N; i++) par[i] = -1;
+       for (int i = 1; i <= N; i++) siz[i] = 1;
+   }
+
+   int root(int x) {
+       while (true) {
+           if (par[x] == -1) break;
+           x = par[x];
+       }
+       return x;
+   }
+
+   void unite(int u, int v) {
+       int rootU = root(u);
+       int rootV = root(v);
+       if (rootU == rootV) return;
+       if (siz[rootU] < siz[rootV]) {
+           par[rootU] = rootV;
+           siz[rootV] = siz[rootU] + siz[rootV];
+       }
+       else {
+           par[rootV] = rootU;
+           siz[rootU] = siz[rootU] + siz[rootV];
+       }
+   }
+
+   bool same(int u, int v) {
+       if (root(u) == root(v)) return true;
+       return false;
+   }
+};
+
+int main() {
+
+    int n; cin >> n;
+    vector<long long> a(n); rep(i, n) cin >> a[i];
+
+    vector<long long> na = a;
+    sort(na.begin(), na.end());
+    na.erase(unique(na.begin(), na.end()), na.end());
+    
+    for (int i = 0; i < n; i++) {
+        long long s = a[i];
+        auto itr = lower_bound(na.begin(), na.end(), s);
+        long long pos = itr - na.begin() + 1;
+
+        cout << pos << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ tessoku/s/a/a17.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = n-1; i >= 0; i--)
+
+vector<char> UPPERCASE = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+vector<char> LOWERCASE = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+void print() {
+  cout << endl;
+}
+
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(tail) != 0) cout << ' ';
+  print(forward<Tail>(tail)...);
+}
+
+template <class T>
+void print(vector<T> &vec) {
+  for (auto& a : vec) {
+    cout << a;
+    if (&a != &vec.back()) cout << ' ';
+  }
+  cout << endl;
+}
+
+template <class T>
+void print(vector<vector<T>> &df) {
+  for (auto& vec : df) {
+    print(vec);
+  }
+}
+
+class UnionFind {
+   public:
+   int par[100009];
+   int siz[100009];
+
+   void init(int N) {
+       for (int i = 1; i <= N; i++) par[i] = -1;
+       for (int i = 1; i <= N; i++) siz[i] = 1;
+   }
+
+   int root(int x) {
+       while (true) {
+           if (par[x] == -1) break;
+           x = par[x];
+       }
+       return x;
+   }
+
+   void unite(int u, int v) {
+       int rootU = root(u);
+       int rootV = root(v);
+       if (rootU == rootV) return;
+       if (siz[rootU] < siz[rootV]) {
+           par[rootU] = rootV;
+           siz[rootV] = siz[rootU] + siz[rootV];
+       }
+       else {
+           par[rootV] = rootU;
+           siz[rootU] = siz[rootU] + siz[rootV];
+       }
+   }
+
+   bool same(int u, int v) {
+       if (root(u) == root(v)) return true;
+       return false;
+   }
+};
+
+int main() {
+
+    int n; cin >> n;
+    vector<int> a(n+1), b(n+1);
+    for (int i = 2; i <= n; i++) cin >> a[i];
+    for (int i = 3; i <= n; i++) cin >> b[i];
+
+    vector<int> dp(n+1);
+    dp[1] = 0; dp[2] = a[2];
+    for (int i = 3; i <= n; i++) dp[i] = min(dp[i-1] + a[i], dp[i-2] + b[i]);
+    
+    vector<int> ans;
+    int pos = n;
+    while (pos >= 1) {
+        ans.push_back(pos);
+        if (dp[pos] == dp[pos-1] + a[pos]) pos -= 1;
+        else pos -= 2;
+    }
+
+    reverse(ans.begin(), ans.end());
+    cout << ans.size() << endl;
+    for (int i = 0; i < ans.size(); i++) cout << ans[i] << " ";
+    cout << endl;
+
+    return 0;
+}
