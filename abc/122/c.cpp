86f0463728eeb31ff992b93fb0e72a2cf570dc2c
Shion <shion24stat@gmail.com>
2022-12-03 01:10:54 +0900

    add many

---
+++ abc/122/c.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = n-1; i >= 0; i--)
+
+vector<char> UPPERCASE = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+vector<char> LOWERCASE = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+void print() {
+  cout << endl;
+}
+
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(tail) != 0) cout << ' ';
+  print(forward<Tail>(tail)...);
+}
+
+template <class T>
+void print(vector<T> &vec) {
+  for (auto& a : vec) {
+    cout << a;
+    if (&a != &vec.back()) cout << ' ';
+  }
+  cout << endl;
+}
+
+template <class T>
+void print(vector<vector<T>> &df) {
+  for (auto& vec : df) {
+    print(vec);
+  }
+}
+
+class UnionFind {
+   public:
+   int par[100009];
+   int siz[100009];
+
+   void init(int N) {
+       for (int i = 1; i <= N; i++) par[i] = -1;
+       for (int i = 1; i <= N; i++) siz[i] = 1;
+   }
+
+   int root(int x) {
+       while (true) {
+           if (par[x] == -1) break;
+           x = par[x];
+       }
+       return x;
+   }
+
+   void unite(int u, int v) {
+       int rootU = root(u);
+       int rootV = root(v);
+       if (rootU == rootV) return;
+       if (siz[rootU] < siz[rootV]) {
+           par[rootU] = rootV;
+           siz[rootV] = siz[rootU] + siz[rootV];
+       }
+       else {
+           par[rootV] = rootU;
+           siz[rootU] = siz[rootU] + siz[rootV];
+       }
+   }
+
+   bool same(int u, int v) {
+       if (root(u) == root(v)) return true;
+       return false;
+   }
+};
+
+int main() {
+
+    int n, q; cin >> n >> q;
+    string s; cin >> s;
+
+    vector<int> cum(n, 0);
+    for (int i = 1; i < n; i++) {
+        if (s[i-1] == 'A' && s[i] == 'C') cum[i] += 1;
+    }
+    for (int i = 1; i < n; i++) cum[i] += cum[i-1];
+    
+    int l, r, il, ir;
+    for (int i = 0; i < q; i++) {
+        cin >> il >> ir;
+        l = il - 1; r = ir - 1;
+        if (l == 0) cout << cum[r] << endl;
+        else {
+            if (s[l] == 'C') {
+                cout << cum[r] - cum[l-1] - 1 << endl;
+            }
+            else {
+                cout << cum[r] - cum[l-1] << endl;
+            }
+        }
+    }
+
+    return 0;
+}
